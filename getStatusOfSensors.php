18e0f789c91801258552299792390774bf8abb12
silver_925@ukr.net <silver_925@ukr.net>
2015-09-25 23:47:54 +0300

    separate method for getting inside temperature to independent file, add methods for writing outside sensors data in database

--- getStatusOfSensors.php
+++ getStatusOfSensors.php
@@ -1,37 +1,6 @@
 
 <?php
-  $json = null;
-    $json = file_get_contents("http://192.168.1.100/sensors/temperature");
-  $data = json_decode($json);
-  $temperature =  $data->data;
-  echo $temperature;
-  echo "C \n";
-
-  $json2 = file_get_contents("http://192.168.1.100/sensors/humidity");
-
-    $data2 = json_decode($json2);
-    $humidity = $data2->data;
-
-  echo $humidity;
-  echo "%";
-$servername = "localhost";
-$username = "root";
-$password = "1111";
-$dbname = "SENSORS_DATA";
-
-// Create connection
-$date = date("Y-m-d H:i:s");
-$conn = mysqli_connect($servername, $username, $password, $dbname);
-$query = "INSERT INTO SENSORS VALUES(0, '$temperature', '$humidity', '$date');";
-// Check connection
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-}
-echo "Connected successfully";
-if (mysqli_query($conn, $query)) {
-    echo "Data inserted successfully";
-} else {
-    echo "Error creating database: " . mysqli_error($conn);
-}
+include "scripts/writeInsideSensorsDataInDatabase.php";
+include "scripts/writeOutsideSensorsDataInDatabase.php";
 
 ?>
