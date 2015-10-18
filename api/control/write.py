0ef351ec6db642e8fc8fd7a1d81fde5fe5604aee
silver_925@ukr.net <silver_925@ukr.net>
2015-10-18 16:04:00 +0300

    try to fix python script

--- api/control/write.py
+++ api/control/write.py
@@ -2,7 +2,9 @@ import serial
 import sys
 import time
 ser = serial.Serial("/dev/ttyUSB0", 9600)
+ser.close();
+ser.open();
 if len (sys.argv) > 1:    
    ser.write(sys.argv[1])
 time.sleep(1);
-	
+ser.close();
