1d752c0601a601f6752ce4c9d0059830a61373c0
silver_925@ukr.net <silver_925@ukr.net>
2015-10-22 22:57:01 +0300

    change function for control led flash

--- api/control/toogle-led-light.php
+++ api/control/toogle-led-light.php
@@ -1,4 +1,8 @@
 <?php
+$json = file_get_contents("http://192.168.1.100/controll/led");
+header('Content-type: application/json');
+echo $json;
+/*
 	if($_GET['brightness']){
 		$s = "python write.py " . $_GET['brightness'];
 		shell_exec($s);
@@ -6,4 +10,5 @@
 	} else {
 	  	shell_exec("python write.py 0");
 	}
+*/
 ?>
