5950d9e343c4ff5ce1168db2ca42420f962849b6
silver_925@ukr.net <silver_925@ukr.net>
2015-10-25 01:58:26 +0300

    add api to view server and pc statuses

---
+++ api/status/home-pc.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Назар
+ * Date: 25.10.2015
+ * Time: 1:56
+ */
+
+$json = file_get_contents("http://192.168.1.100/status/home-pc");
+header('Content-type: application/json');
+echo $json;
+?>
