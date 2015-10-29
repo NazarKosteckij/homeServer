202b013a5939b1dfc7ebbd06256c699644e198c8
silver_925@ukr.net <silver_925@ukr.net>
2015-10-29 23:43:51 +0200

    fixing api

--- api/sensors/inside/temperature.php
+++ api/sensors/inside/temperature.php
@@ -5,7 +5,12 @@
  * Date: 16.10.2015
  * Time: 19:59
  */
-include "../../../services/InsideSensorsService.php";
-$outside = new InsideSensorsService();
-echo $outside->getTemperature();
+$json = file_get_contents("http://192.168.1.100/sensors/temperature");
+$data = json_decode($json);
+$temperature =  $data->data;
+if (is_null($temperature)) {
+    $temperature = "NaN";
+}
+echo $temperature;
+
 ?>
