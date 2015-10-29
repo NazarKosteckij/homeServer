68b61fb969cdb079b05fbc4a86b232a93591dfe5
silver_925@ukr.net <silver_925@ukr.net>
2015-10-29 23:45:42 +0200

    fixing api

--- api/sensors/outside/humidity.php
+++ api/sensors/outside/humidity.php
@@ -5,7 +5,11 @@
  * Date: 16.10.2015
  * Time: 19:59
  */
-namespace sensors;
-$outside = new OutsideSensorsService();
-echo $outside->getHumidity();
+$json = file_get_contents("http://192.168.1.100/sensors/humidity");
+$data = json_decode($json);
+$humidity =  $data->data;
+if (is_null($humidity)) {
+    $humidity = "NaN";
+}
+echo $humidity;
 ?>
