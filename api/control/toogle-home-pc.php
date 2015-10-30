0010f0b5019f8b3c17eef703b29e38b1ab696062
silver_925@ukr.net <silver_925@ukr.net>
2015-10-30 20:58:08 +0200

    continue refactoring api

--- api/control/toogle-home-pc.php
+++ api/control/toogle-home-pc.php
@@ -1,4 +1,5 @@
 <?php
+	header('Content-Type:application/json');
 	$json = file_get_contents("http://192.168.1.100/start_home-pc");
 	header('Content-type: application/json');
 	echo $json;
--- api/status/led-light.php
+++ api/status/led-light.php
@@ -1,10 +1,11 @@
 <?php
 /**
  * Created by PhpStorm.
- * User: Назар
+ * User: пїЅпїЅпїЅпїЅпїЅ
  * Date: 22.10.2015
  * Time: 23:38
  */
+header('Content-Type:application/json');
 $json = file_get_contents("http://192.168.1.100/status/led-light");
 header('Content-type: application/json');
 echo $json;
