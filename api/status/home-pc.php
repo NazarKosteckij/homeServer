dc46f12144a6915e4d4edb9d21c5d1d3f0ab217c
silver_925@ukr.net <silver_925@ukr.net>
2015-11-01 15:42:55 +0200

    add  ArduinoConnector (cherry picked from commit 6bcde9e)

--- api/status/home-pc.php
+++ api/status/home-pc.php
@@ -5,8 +5,6 @@
  * Date: 25.10.2015
  * Time: 1:56
  */
-include("ArduinoConnector.php");
-
 header('Content-Type:application/json');
 $arduinoConnector = new ArduinoConnector();
 echo $arduinoConnector->doRequestToArduino("http://192.168.1.100/status/Home-PC");
