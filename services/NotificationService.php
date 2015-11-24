13842b426676d03e8306fe043e1172bf76b68f8a
server <silver_925@ukr.net>
2015-11-24 20:32:05 +0200

    implement notification to email

--- services/NotificationService.php
+++ services/NotificationService.php
@@ -1,9 +1,9 @@
 <?php
  class NotificationService{
 
-	function sendNotification(){
-		
-		
+	function sendNotification($text){
+		$text = str_replace("%20", " ", $text);
+		file_get_contents("http://192.168.1.5:8080/api/notifications?email=silver_925@ukr.net&message=$text");
 	}
 
 
